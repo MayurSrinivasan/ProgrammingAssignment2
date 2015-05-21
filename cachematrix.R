6fc004fbb106abedb7a7ca650e938e5c43a46291
MayurSrinivasan <srinivasan.mayur@gmail.com>
2015-05-22 03:10:13 +0530

    Changed R file appropriately

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix makes an object that contains functions that get/set an invertible Matrix and its Inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+
+  m<- NULL ## initialize inverse
+  
+  set <- function(y) {
+    x <<- y 
+    m<<- NULL
+  }
+  
+  get <- function() x
+  
+  
+  setInv <- function(invert) {
+    m <<- invert 
+    return m
+  }
+  
+  getInv  <- function() m
+  list(set=set, get=get, setInv=setInv, getInv=getInv)
 
 }
 
 
-## Write a short comment describing this function
+## If an inverted Matrix is found in cache, it is assigned, else Inverse is calculated through Solve function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+finalInv<- x$getInv() 
+  
+
+  if(!is.null(finalInv)) { 
+    return(finalInv)
+  }
+  
+  mat<- x$get()  
+  
+  finalInv <- solve(mat)
+  
+  x$setInv(finalInv)
+
+
+
 }
